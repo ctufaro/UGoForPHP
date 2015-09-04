95de1a649067ba6dc5ccfd613ad2e8b8b29f8c6e
Christopher Tufaro <ctufaro@gmail.com>
2015-09-04 10:31:09 -0400

    Update retrieve.php

--- retrieve.php
+++ retrieve.php
@@ -3,5 +3,5 @@
   $pieces = explode("UDID", $data);
   $pieces2 = explode("%3C%2Fstring", $pieces[1]);
   $pieces3 = explode("3Cstring%3E", $pieces2[0]);
-  header("Location: http://ugoforapi.azurewebsites.net/home/udid?data=".rawurlencode($pieces2[1]), true, 301);
+  header("Location: http://ugoforapi.azurewebsites.net/home/udid?data=".rawurlencode($pieces[1]), true, 301);
 ?>
