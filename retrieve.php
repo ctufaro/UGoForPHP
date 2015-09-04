9dae620495b8dce1d33eb57fa6cc9d21371cb391
Christopher Tufaro <ctufaro@gmail.com>
2015-09-04 09:41:00 -0400

    Update retrieve.php

--- retrieve.php
+++ retrieve.php
@@ -1,4 +1,4 @@
 <?php
   $data = file_get_contents('php://input');
-  header("Location: http://ugoforapi.azurewebsites.net/home/udid?data=".rawurlencode($data), true, 301);
+  header("Location: http://ugoforphp.azurewebsites.net/udid?data=".rawurlencode($data), true, 301);
 ?>
