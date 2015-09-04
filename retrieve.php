03a621a15e1e9292b8bcd9e13f4a276b38ba9dee
Christopher Tufaro <ctufaro@gmail.com>
2015-09-04 10:03:18 -0400

    Update retrieve.php

--- retrieve.php
+++ retrieve.php
@@ -1,6 +1,5 @@
 <?php
   $data = file_get_contents('php://input');
   $pieces = explode("UDID", $data);
-  $pieces2 = explode("Fstring", $pieces[1])
-  header("Location: http://ugoforphp.azurewebsites.net/udid?data=".urlencode($pieces2[0]), true, 301);
+  header("Location: http://ugoforphp.azurewebsites.net/udid?data=".urlencode($pieces[1]), true, 301);
 ?>
