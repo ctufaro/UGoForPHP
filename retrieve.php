1d28102c9c12899e110b69ba7e77b0ba22e1ef07
christufaro <ctufaro@gmail.com>
2015-09-04 06:10:44 -0400

    bullshit php

--- retrieve.php
+++ retrieve.php
@@ -1,4 +1,4 @@
 <?php
   $data = file_get_contents('php://input');
-  header("Location: http://ugoforapi.azurewebsites.net/home/udid?data=".rawurlencode($data));
+  header("Location: http://ugoforphp.azurewebsites.net?data=".rawurlencode($data));
 ?>
